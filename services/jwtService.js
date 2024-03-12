52414071f4f6cde331dee945ec2b2678f341a31d
diana080299 <132782216+diana080299@users.noreply.github.com>
2024-03-12 19:59:51 +0000

    upd

--- services/jwtService.js
+++ services/jwtService.js
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
 import HttpError from '../helpers/HttpError.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
