43236b12d0518b1f84d6464b7b61c2903f31fb5a
diana080299 <132782216+diana080299@users.noreply.github.com>
2024-03-17 19:17:34 +0000

    up

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,23 +1,14 @@
 const messageList = {
-    400: "Bad Request",
-    401: "Unauthorized",
-    403: "Forbidden",
-    404: "Not Found",
-    409: "Conflict",
-}
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'Not Found',
+  409: 'Conflict',
+};
 
 const HttpError = (status, message = messageList[status]) => {
-<<<<<<< Updated upstream
-    const error = new Error(message);
-    error.status = status;
-    return error;
-}
-
-export default HttpError;
-=======
-  const error = Error(message);
+  const error = new Error(message);
   error.status = status;
   return error;
 };
 export default HttpError;
->>>>>>> Stashed changes
