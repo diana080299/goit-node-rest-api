ef04724c44fa8970d5d634460c56d4afa755f26d
diana080299 <132782216+diana080299@users.noreply.github.com>
2024-03-25 15:22:53 +0000

    update

--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,8 @@
-import { User } from '../model/userModel.js';
-import { jwtService } from '../services/jwtService.js';
-import HttpError from '../helpers/HttpError.js';
-import { ImageService } from '../services/imageService.js';
-import path from 'path';
+import { User } from "../model/userModel.js";
+import { jwtService } from "../services/jwtService.js";
+import HttpError from "../helpers/HttpError.js";
+import { ImageService } from "../services/imageService.js";
+import { sendEmail } from "../services/emailService.js";
 
 export const signup = async (userData) => {
   const user = await User.create({
@@ -22,44 +22,71 @@ export const signup = async (userData) => {
   return newUser;
 };
 
+export const verify = async (verificationToken) => {
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+  await User.findOneAndUpdate(user._id, {
+    verify: true,
+    verificationToken: true,
+  });
+};
+export const resendEmail = async (email) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+  if (user.verify) throw HttpError(400, "Verification has already been passed");
+
+  const verifyEmail = {
+    to: user.email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${user.verificationToken}" >Click verify email</a>`,
+  };
+  await sendEmail(verifyEmail);
+};
 export const checkUserExists = async (filter) => {
   const userExists = await User.exists(filter);
   return userExists;
 };
-
 export const updateUserToken = (id, body) => User.findByIdAndUpdate(id, body);
 export const login = async ({ email, password }) => {
-  const user = await User.findOne({ email }).select('+password');
+  const user = await User.findOne({ email }).select("+password");
 
-  if (!user) throw HttpError(401, 'Email or password is wrong');
-
+  if (!user) throw HttpError(401, "Email or password is wrong");
+  if (!user.verify) throw HttpError(404, "User not found");
   const isPasswordValid = await user.checkPassword(password, user.password);
 
-  if (!isPasswordValid) throw HttpError(401, 'Email or password is wrong..');
-
+  if (!isPasswordValid) throw HttpError(401, "Email or password is wrong..");
+  const token = jwtService.signToken(user._id);
+  user.token = token;
+  await user.save();
   return user;
 };
 export const getUserById = (id) => User.findById(id);
 
 export const logout = async (token) => {
   if (!token) {
-    throw new HttpError(401, 'Not authorized');
+    throw HttpError(401, "Not authorized 6");
   }
   const userId = jwtService.checkToken(token);
 
-  if (!userId) throw HttpError(401, 'Not authorized');
+  if (!userId) throw HttpError(401, "Not authorized 7 ");
 
   const currentUser = await getUserById(userId);
 
-  if (!currentUser) throw HttpError(401, 'Not authorized');
+  if (!currentUser) throw HttpError(401, "Not authorized 8");
 
   currentUser.token = null;
+  console.log(currentUser);
   await currentUser.save();
+  console.log(currentUser.save());
   return;
 };
-
 export const updateMe = async (userData, user, file) => {
-  if (!file) throw HttpError(400, 'Please, add the file');
+  if (!file) throw HttpError(400, "Please, add the file");
   else {
     user.avatarURL = await ImageService.saveImage(
       file,
@@ -69,8 +96,8 @@ export const updateMe = async (userData, user, file) => {
         height: 250,
       },
       user,
-      'public',
-      'avatars'
+      "public",
+      "avatars"
     );
   }
 
@@ -81,4 +108,4 @@ export const updateMe = async (userData, user, file) => {
   return user.save();
 };
 
-export * as userService from './userService.js';
+export * as userService from "./userService.js";
